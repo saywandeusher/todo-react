1f0113447605285a035fc814da07b057aa17ab9d
saywandeusher <ongsaywan@hotmail.com>
2018-08-02 17:25:25 +0800

    trying stuff

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 class List extends React.Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.changeHandler = this.changeHandler.bind( this );
-  }
-
-  state = {
-    list : [],
-    word : ""
+    this.state = {
+      list : [],
+      word : ""
+    }
   }
 
   changeHandler(event){
     this.setState({word:event.target.value});
     console.log("change", event.target.value);
+    
+    var newItem = {
+      text: this._inputElement.value,
+      key: Date.now()
+    };
+
+    console.log(newItem);
   }
 
   render() {
       // render the list with a map() here
-
       console.log("rendering");
       return (
         <div className="list">
-          <input onChange={this.changeHandler} value={this.state.word}/>
-          <button>add item</button>
+          <input onChange={this.changeHandler} value={this.state.word} placeholder="enter task" ref={(a) => this._inputElement = a}/>
+          <button type="submit">add item</button>
         </div>
       );
   }
@@ -31,4 +36,3 @@ ReactDOM.render(
     <List/>,
     document.getElementById('root')
 );
-
