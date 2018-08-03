81f36e0547148e9079e35b559628b34c73ed72c0
saywandeusher <ongsaywan@hotmail.com>
2018-08-03 09:48:32 +0800

    list keeps refreshing after rendering :(

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 class List extends React.Component {
   constructor(props){
     super(props)
-    this.changeHandler = this.changeHandler.bind( this );
+    this.changeHandler = this.changeHandler.bind(this);
+    this.handleClick = this.handleClick.bind( this );
     this.state = {
       list : [],
       word : ""
@@ -9,24 +10,41 @@ class List extends React.Component {
   }
 
   changeHandler(event){
+    // if (event.target.value.length < 1 ) {
+    // this.state.enterSmth = 'Enter something';
+    // } else {
+    //   this.state.enterSmth = "";
+    // }
     this.setState({word:event.target.value});
     console.log("change", event.target.value);
-    
-    var newItem = {
-      text: this._inputElement.value,
-      key: Date.now()
-    };
+  }
 
-    console.log(newItem);
+  handleClick(){
+    let task = this.state.word;
+    if (task.length < 1) {
+      alert("Please enter a task!");
+    } else {
+      this.state.list.push(task);
+      console.log(this.state.list);
+      let currentList = this.state.list;
+      this.setState({list:currentList})
+    }
   }
 
   render() {
       // render the list with a map() here
+      let renderedList = this.state.list.map( (item, index) => {
+                        return <li key={index}>{item}</li>
+      })
+      console.log(renderedList);
       console.log("rendering");
       return (
         <div className="list">
-          <input onChange={this.changeHandler} value={this.state.word} placeholder="enter task" ref={(a) => this._inputElement = a}/>
+        <form onSubmit={this.handleClick}>
+          <input onChange={this.changeHandler} value={this.state.word} placeholder="enter task" />
           <button type="submit">add item</button>
+          </form>
+          <div>{renderedList}</div>
         </div>
       );
   }
